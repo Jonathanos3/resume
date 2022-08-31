3f2f8d4c50b00354bb68c3d0a500416eefdffb0b
Jonathan Oliveira <jonathanoliveira2@hotmail.com>
2022-08-31 20:32:18 -0300

    commit.

--- components/Summary.js
+++ components/Summary.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Summary = () => {
   return (<div className='bg-white rounded-lg shadow-lg py-12 px-8 mx-8 md:mx-0 md:px-16'>
     <p className='text-2xl'> Graduated in Information Systems, I am looking for Opportunity in the Developer Area.
-      I'm currently taking a Fullstack course at (devpleno.com) in which I work with various types of technologies, such as: NodeJS, React, Github, NextJs, JavaScript.</p>
+      I'm currently taking a Fullstack course at (devpleno.com) in which I work with various types of technologies, such as: NodeJS, React, GitHub, NextJS, JavaScript.</p>
   </div>)
 }
 
